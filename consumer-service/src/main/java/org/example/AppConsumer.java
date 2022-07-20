3b61ca9648fe3900c7ad70a933bf33becb1b7c07
1757168240 <liu199399>
2022-07-20 18:06:33 +0800

    修改pom

---
+++ consumer-service/src/main/java/org/example/AppConsumer.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsumer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConsumer.class);
+    }
+}
---
+++ provider-service/src/main/java/org/example/AppProvider.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProvider {
+    public static void main(String[] args) {
+        SpringApplication.run(AppProvider.class);
+    }
+}
